4239606fe1228f6dd439cbc4d965a2599523533c
senzonzuza <senzokaqhaka@gmail.com>
2024-06-21 09:52:04 +0200

    Model created

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+	public class Category
+	{
+		[Key]
+		public int Id { get; set; }
+		public required string Name { get; set; }
+		public int DisplayOrder { get; set; }
+	}
+}
+
