366c87fa492c31891f2fdcec709bc4edc99cdd89
senzonzuza <senzokaqhaka@gmail.com>
2024-06-26 15:19:20 +0200

    validation done

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace BulkyWeb.Models
@@ -7,8 +8,12 @@ namespace BulkyWeb.Models
 	{
 		[Key]
 		public int Id { get; set; }
+		[DisplayName("Name")]
+		[MaxLength(30)]
 		public required string Name { get; set; }
-		public int DisplayOrder { get; set; }
+        [DisplayName("DisplayOrder")]
+		[Range(1,100,ErrorMessage ="Display Order must be between 1-100" )]
+        public int DisplayOrder { get; set; }
 	}
 }
 
