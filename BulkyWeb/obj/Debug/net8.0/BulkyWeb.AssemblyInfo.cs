512a89c15aacef79c35b93bae2d3865ff3dcfe77
senzonzuza <senzokaqhaka@gmail.com>
2024-06-26 11:16:52 +0200

    bootswatch theme added

--- BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e16ba3e12d039f002f97193eb8ad1e004c20c93b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ba3ffad7fe8975c29e5e38546dc1da550e4988b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
