8ccc60ea1ccc9846ca55d2067330f663b2ca0d03
senzonzuza <senzokaqhaka@gmail.com>
2024-07-03 12:43:24 +0200

    register service

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Senzo.DataAccess.Data;
+using Senzo.DataAccess.Repository;
+using Senzo.DataAccess.Repository.IRepository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(option =>
 option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
 var app = builder.Build();
 
